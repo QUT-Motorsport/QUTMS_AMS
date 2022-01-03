b8cf8e42f5f02e03a961c95d43715dc5cf5c6920
Calvin <calvinofthejs@gmail.com>
2022-01-04 00:12:13 +1000

    start setting up bms handler

--- Software/Core/Inc/bms.h
+++ Software/Core/Inc/bms.h
@@ -10,10 +10,23 @@
 
 #include <Timer.h>
 
+typedef struct bms_status {
+	uint16_t voltages[BMS_COUNT][BMS_VOLTAGE_COUNT];
+	uint8_t temperatures[BMS_COUNT][BMS_TEMPERATURE_COUNT];
+
+	uint8_t lastVoltMsgIdx[BMS_COUNT];
+} bms_status_t;
+
 extern ms_timer_t bms_timer;
+extern bms_status_t bms;
 
 void bms_ctrl_off();
 void bms_ctrl_on();
+
+void bms_CAN_timer_cb(void *args);
+
+void bms_handleVoltageMsg(CAN_MSG_Generic_t *msg);
+void bms_handleTemperatureMsg(CAN_MSG_Generic_t *msg);
 
 
 #endif /* INC_BMS_H_ */
--- Software/Core/Src/bms.c
+++ Software/Core/Src/bms.c
@@ -7,8 +7,10 @@
 
 #include "bms.h"
 #include "main.h"
+#include "QUTMS_CAN.h"
 
 ms_timer_t bms_timer;
+bms_status_t bms;
 
 void bms_ctrl_off() {
 	HAL_GPIO_WritePin(BMS_CTRL_GPIO_Port, BMS_CTRL_Pin, GPIO_PIN_RESET);
@@ -16,4 +18,39 @@ void bms_ctrl_off() {
 
 void bms_ctrl_on() {
 	HAL_GPIO_WritePin(BMS_CTRL_GPIO_Port, BMS_CTRL_Pin, GPIO_PIN_SET);
+}
+
+void bms_CAN_timer_cb(void *args) {
+	CAN_MSG_Generic_t msg;
+	while (queue_next(&queue_CAN_BMS, &msg)) {
+		uint32_t masked_id = (msg.ID & ~0xF);
+		uint8_t idx = (msg.ID & 0xF);
+
+		if (idx < 1 || idx >= BMS_COUNT) {
+			printf("Invalid BMS ID: %d, MSG: 0x%08X\r\n", idx);
+		} else {
+			if (masked_id == BMS_TransmitVoltage_ID) {
+				bms_handleVoltageMsg(&msg);
+			} else if (masked_id == BMS_TransmitTemperature_ID) {
+				bms_handleTemperatureMsg(&msg);
+			} else if (masked_id == BMS_BadCellVoltage_ID) {
+
+			} else if (masked_id == BMS_BadCellTemperature_ID) {
+
+			}
+		}
+
+	}
+}
+
+void bms_handleVoltageMsg(CAN_MSG_Generic_t *msg) {
+	uint8_t idx = (msg->ID & 0xF);
+	uint8_t msgId;
+	uint16_t voltages[4];
+
+	Parse_BMS_TransmitVoltage(msg->data, &msgId, voltages);
+}
+
+void bms_handleTemperatureMsg(CAN_MSG_Generic_t *msg) {
+	uint8_t idx = (msg->ID & 0xF);
 }
