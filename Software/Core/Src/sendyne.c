09bdd094cba42ef6f0e797bde70a14a26f83b012
Calvin <calvinofthejs@gmail.com>
2022-01-10 17:08:30 +1000

    fix sendyne message request

--- Software/Core/Src/sendyne.c
+++ Software/Core/Src/sendyne.c
@@ -95,7 +95,7 @@ void sendyne_CAN_timer_cb(void *args) {
 }
 
 void sendyne_voltage_timer_cb(void *args) {
-	Sendyne_RequestData_t msg = Compose_Sendyne_RequestData(1, CS_V1);
+	Sendyne_RequestData_t msg = Compose_Sendyne_RequestData(0, CS_V1);
 
 	CAN_TxHeaderTypeDef header = { .ExtId = msg.id, .IDE =
 	CAN_ID_EXT, .RTR = CAN_RTR_DATA, .DLC = sizeof(msg.data),
@@ -106,7 +106,7 @@ void sendyne_voltage_timer_cb(void *args) {
 }
 
 void sendyne_current_timer_cb(void *args) {
-	Sendyne_RequestData_t msg = Compose_Sendyne_RequestData(1, CS_CURRENT);
+	Sendyne_RequestData_t msg = Compose_Sendyne_RequestData(0, CS_CURRENT);
 
 	CAN_TxHeaderTypeDef header = { .ExtId = msg.id, .IDE =
 	CAN_ID_EXT, .RTR = CAN_RTR_DATA, .DLC = sizeof(msg.data),
@@ -115,7 +115,9 @@ void sendyne_current_timer_cb(void *args) {
 	// send request
 	AMS_send_can_msg(&CANBUS4, &header, msg.data);
 
-	msg = Compose_Sendyne_RequestData(2, CS_CURRENT);
+	HAL_Delay(1);
+
+	msg = Compose_Sendyne_RequestData(1, CS_CURRENT);
 	header.ExtId = msg.id;
 
 	// send request
